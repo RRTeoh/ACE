104529f74fd41b1fc6f4ec029fccbf012d17988d
RRTeoh <rrteoh1103@gmail.com>
2026-03-24 09:58:11 +0800

    setup

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById('action-button');
+button.addEventListener('click', () => {
+  alert('🚀 ACE website scaffolded successfully!');
+});
+
+console.log('ACE website ready');
